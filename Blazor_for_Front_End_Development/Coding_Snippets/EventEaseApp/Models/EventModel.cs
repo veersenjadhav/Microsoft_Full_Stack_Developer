3b1de4e44e608a2039d1d44305f7360d2d7c1db8
Veersen Jadhav <veersenjadhav@gmail.com>
2026-05-26 17:38:15 +0530

    EventEaseApp - Activity 1 - Creating and re-using Event Card for multiple pages

---
+++ Blazor_for_Front_End_Development/Coding_Snippets/EventEaseApp/Models/EventModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EventEaseApp.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
