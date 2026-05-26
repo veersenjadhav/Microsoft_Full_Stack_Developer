99123411b045f3de67c7da92d757db2923af8a89
Veersen Jadhav <veersenjadhav@gmail.com>
2026-05-26 18:19:40 +0530

    EventEaseApp - Activity 2 - Added Create New Event Functionality

--- Blazor_for_Front_End_Development/Coding_Snippets/EventEaseApp/Models/EventModel.cs
+++ Blazor_for_Front_End_Development/Coding_Snippets/EventEaseApp/Models/EventModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace EventEaseApp.Models
 {
@@ -8,5 +9,13 @@ namespace EventEaseApp.Models
         public string Name { get; set; }
         public DateTime Date { get; set; }
         public string Location { get; set; }
+
+        // Mock database for sharing state across pages
+        public static List<EventModel> Events { get; set; } = new List<EventModel>
+        {
+            new EventModel { Id = 1, Name = "Annual Tech Conference", Date = DateTime.Today.AddDays(30), Location = "Convention Center" },
+            new EventModel { Id = 2, Name = "Music Festival", Date = DateTime.Today.AddDays(45), Location = "City Park" },
+            new EventModel { Id = 3, Name = "Art Expo", Date = DateTime.Today.AddDays(60), Location = "Downtown Gallery" }
+        };
     }
 }
