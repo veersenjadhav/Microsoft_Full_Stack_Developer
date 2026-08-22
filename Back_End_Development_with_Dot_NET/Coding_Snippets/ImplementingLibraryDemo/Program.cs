b5ff3691882b3f45120c6c3de84f06252283de67
Veersen Jadhav <veersenjadhav@gmail.com>
2026-08-22 13:19:39 +0530

    Sample Code for Serielization and Deserielization of JSON data - Using Library Demo

--- Back_End_Development_with_Dot_NET/Coding_Snippets/ImplementingLibraryDemo/Program.cs
+++ Back_End_Development_with_Dot_NET/Coding_Snippets/ImplementingLibraryDemo/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.Common;
+using Newtonsoft.Json;
+
+public class Person
+{
+    public int id {get; set;}
+
+    public string? name {get; set;}
+
+    public bool isActive {get; set;}
+
+    public string[]? roles {get; set;}
+
+    public Dictionary<string, string>? location {get; set;}
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Serielized string data
+        string? personSerielisedData = "{\"id\":101,\"name\":\"Alice Smith\",\"isActive\":true,\"roles\":[\"admin\",\"user\"],\"location\":{\"city\":\"Austin\",\"state\":\"TX\"}}";
+
+        // Deserielize and store into object
+        Person? lobjPerson = JsonConvert.DeserializeObject<Person>(personSerielisedData);
+
+        Console.WriteLine("Deserielization of JSON Data into C# object...");
+        Console.WriteLine($" Person Name : {lobjPerson.id} \n Has Top Role Assigned : {lobjPerson.roles[0]} \n Location : {lobjPerson.location["city"]}, {lobjPerson.location["state"]}");
+
+        // Serielize and store into string
+        Person? lobjPerson2 = new Person{
+            id = 102,
+            name = "Veersen Jadhav",
+            isActive = true,
+            roles = new string[] {"CEO", "CFO"},
+            location = new Dictionary<string, string>
+            {
+                { "city", "St. Louis" },
+                { "state", "NY" }
+            }
+        };
+
+        string? personSerielisedData2 = JsonConvert.SerializeObject(lobjPerson2);
+
+        Console.WriteLine("\n Serielization of Data - C# object into JSON string...");
+        Console.WriteLine(personSerielisedData2);
+    }
+}
